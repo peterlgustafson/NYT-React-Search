0990c132dd41310562c4ac9bdb5bd3065db0aeb8
Peter Gustafson <peterlideckergustafson@gmail.com>
2018-04-06 17:25:59 -0500

    added react app

--- server.js
+++ server.js
@@ -24,3 +24,53 @@ var db = mongojs(databaseUrl , collections);
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
+
+//Routes
+
+//Route to get all articles
+
+app.get('/api/articles', function(req, res){
+    // res.send('hi');
+    db.articles.find({
+      "_id": mongojs.ObjectID(req.params.id)
+    }, function(error, result){
+        res.json(result);
+    });
+  });
+
+//Route to post article
+app.post('/api/articles', function(req, res){
+	db.articles.insert(req.body, function(error, savedArticle) {
+	  // Log any errors
+	  if (error) {
+	    res.send(error);
+	  }else {
+	    res.json(savedArticle);
+	  }
+	});
+});
+
+//Route to delete article from a database
+app.delete('/api/articles/:id', function (req, res) {
+
+    var article_id = req.params.id;
+  
+    db.articles.remove({
+      "_id": mongojs.ObjectID(article_id)
+    }, function (error, removed) {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(article_id);
+      }
+    });
+  });
+
+  //De-facto catch all route
+//   app.get('*', function(req, res){
+//     res.sendFile(__dirname + './client/build/index.html');
+
+// Listen on port 3001
+app.listen(PORT, function() {
+    console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
+  });
