0269bd10f1802d454cf135f7c56279f20ae3b226
Peter Gustafson <peterlideckergustafson@gmail.com>
2018-04-09 14:49:24 -0500

    updated with search working

--- nytreact/src/App.js
+++ nytreact/src/App.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Results from './results/Results';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      articles: [],
+      search: "",
+      startDate: "",
+      endDate: ""
+    }
+  }
+
+  getArticles = (event) => {
+    event.preventDefault();
+    let end = (this.state.endDate == undefined) ? 0 : this.state.endDate;
+    return fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.state.search}&begin_date=${this.state.startDate}0101&endDate=${this.state.endDate}1231&api-key=0eafe269423a4ff88b474ef6d3ceea7e`)
+      .then(res => res.json())
+  .then(res => this.setState({articles: res.response.docs} ))
+}
+
+  componenentDidMount() {
+    this.getArticles('jazz', '20180101')
+      .then(articles => this.setState({ articles }))
+  }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name
+    this.setState({ [name]: value });
+  };
+
+    saveArticle = (event) => {
+      debugger;
+      const title = event.target.parentElement.children[0].textContent;
+      const snippet = event.target.parentElement.children[1].textContent;
+      const publicationDate = event.target.parentElement.children[2].textContent;
+      const url = event.target.parentElement.children[3].textContent;
+
+        fetch("/api/articles", {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title : title, snippet : snippet, publicationDate : publicationDate, url : url } )})
+        .then(res => res.json())
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://www.ispu.org/wp-content/uploads/2016/08/nyt-circle.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">New York Times Article Scraper</h1>
+          <h3 className="App-subtitle">Search for and annotate articles of interest!</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>Search</h1>
+        <form>
+          <label>Topic</label>
+          <input type="text" name="search" onChange={this.handleInputChange}></input>
+          <label>Start Year</label>
+          <input type="text" name="startDate" onChange={this.handleInputChange}></input>
+          <label>End Year</label>
+          <input type="text" name="endDate" onChange={this.handleInputChange}></input>
+          <input type="submit" onClick={this.getArticles}></input>
+        </form>
+
+
+        <h1>Results</h1>
+          {this.state.articles.map((art, ind) => <ul key={ind}> 
+          <li> {art.headline.main}</li>
+          <li> {art.snippet}</li>
+          <li> {art.pub_date}</li>
+          <li> {art.web_url}</li>
+
+            <button onClick={this.saveArticle}>Save</button>
+</ul>
+)}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -18,59 +18,88 @@ var databaseUrl = process.env.MONGODB_URI || "nytreact_db";
 var collections = ["articles"];
 
 // Hook mongojs config to db variable
-var db = mongojs(databaseUrl , collections);
+var db = mongojs(databaseUrl, collections);
 
 // Log any mongojs errors to console
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
 });
 
 //Routes
 
 //Route to get all articles
 
-app.get('/api/articles', function(req, res){
+app.get('/api/articles', function (req, res) {
     // res.send('hi');
     db.articles.find({
-      "_id": mongojs.ObjectID(req.params.id)
-    }, function(error, result){
+        "_id": mongojs.ObjectID(req.params.id)
+    }, function (error, result) {
         res.json(result);
     });
-  });
+});
+
+//Route for search
+// app.get('/api/articles/:q/:begin_date/q:end_date/', function (req, res) {
+//     var ob = {
+//         'api-key': "0eafe269423a4ff88b474ef6d3ceea7e",
+//         'page': "0",
+//         'q': req.params.q,
+//         'begin_date': req.params.begin_date,
+//     }
+
+//     if (req.params.end_date != 0) {
+//         ob.end_date = req.params.end_date;
+//     }
+
+//     request.get({
+//         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+//         qs: ob,
+//         headers: {
+//             'Accept': 'application/json',
+//             'Origin': '*',
+//             'Content-Type': 'application/json'
+//         },
+//     }, function (err, response, body) {
+//         body = JSON.parse(body);
+//         res.json(body);
+//     })
+// })
+
 
 //Route to post article
-app.post('/api/articles', function(req, res){
-	db.articles.insert(req.body, function(error, savedArticle) {
-	  // Log any errors
-	  if (error) {
-	    res.send(error);
-	  }else {
-	    res.json(savedArticle);
-	  }
-	});
+app.post('/api/articles', function (req, res) {
+    console.log(req.body);
+    db.articles.insert(req.body, function (error, savedArticle) {
+        // Log any errors
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(savedArticle);
+        }
+    });
 });
 
 //Route to delete article from a database
 app.delete('/api/articles/:id', function (req, res) {
 
     var article_id = req.params.id;
-  
-    db.articles.remove({
-      "_id": mongojs.ObjectID(article_id)
-    }, function (error, removed) {
-      if (error) {
-        res.send(error);
-      } else {
-        res.json(article_id);
-      }
-    });
-  });
 
-  //De-facto catch all route
+    db.articles.remove({
+        "_id": mongojs.ObjectID(article_id)
+    }, function (error, removed) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(article_id);
+        }
+    });
+});
+
+//De-facto catch all route
 //   app.get('*', function(req, res){
 //     res.sendFile(__dirname + './client/build/index.html');
 
 // Listen on port 3001
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
-  });
+});
